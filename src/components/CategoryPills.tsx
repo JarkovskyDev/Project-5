7fb10f1169d01a6a3e0fc0dc1d5797b73841a6c6
Dušan Jarkovský <jarkovsky.dev@gmail.com>
2024-01-14 00:14:07 +0100

    54:03

--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import Button from './Button'
-import { ChevronLeft } from 'lucide-react'
+import { ChevronLeft, ChevronRight } from 'lucide-react'
 
 type CategoryPillProps = {
     categories: string[],
@@ -8,15 +8,37 @@ type CategoryPillProps = {
     onSelect: (category: string) => void
 }
 
+const TRANSLATE_AMOUT = 200
+
 const CategoryPills = ( { categories, selectedCategory, onSelect }: CategoryPillProps) => {
+    const [translate, setTranslate] = useState(0)
     const [isLeftVisible, setIsLeftVisible] = useState(false)
     const [isRightVisible, setIsRightVisible] = useState(false)
+    const containerRef = useRef<HTMLDivElement>(null)
+
+useEffect(() => {
+    if (containerRef.current == null) return
+
+    const observer = new ResizeObserver(entries => {
+        const container = entries[0]?.target
+        if (container == null) return
+
+        setIsLeftVisible(translate > 0)
+        setIsRightVisible(translate + container.clientWidth < container.scrollWidth)
+    })
+
+    observer.observe(containerRef.current)
+
+    return () => {
+        observer.disconnect()
+    }
+}, [categories, translate])
 
   return (
     
 
-    <div className='overflow-x-hidden relative'>
-        <div className='flex whitespace-nowrap gap-3 transition-transform w-[max-content]'>
+    <div ref={containerRef} className='overflow-x-hidden relative'>
+        <div className='flex whitespace-nowrap gap-3 transition-transform w-[max-content]' style={{ transform: `translateX(-${translate}px)`}}>
             {categories.map(category => (
             <Button key={category} 
                     onClick={() => onSelect(category)}
@@ -26,12 +48,43 @@ const CategoryPills = ( { categories, selectedCategory, onSelect }: CategoryPill
             </Button>
             ))}
         </div>
-        {isLeftVisible && <div className='absolute left-0 top-1/2 -translate-y-1/2'>
-                <Button variant="ghost" size="icon" className='h-full aspect-square w-auto p-1.5 bg-gradient-to-r from-white from-50% to-transparent w-24 h-full '>
+        {isLeftVisible && (<div className='absolute left-0 top-1/2 -translate-y-1/2'>
+                <Button variant="ghost" 
+                        size="icon" 
+                        className='h-full aspect-square w-auto p-1.5 bg-gradient-to-r from-white from-50% to-transparent w-24 h-full '
+                        onClick={() => {
+                            setTranslate(translate => {
+                                const newTranslate = translate - TRANSLATE_AMOUT
+                                if (newTranslate <= 0) return 0
+                                return newTranslate
+                            })
+                        }}>
                     <ChevronLeft />
                 </Button>
             </div>
-        }
+        )}
+        {isRightVisible && (<div className='absolute right-0 top-1/2 -translate-y-1/2'>
+                <Button variant="ghost" 
+                        size="icon"
+                        className='h-full aspect-square w-auto p-1.5 bg-gradient-to-l from-white from-50% to-transparent w-24 h-full flex justify-end'
+                        onClick={() => {
+                            setTranslate(translate => {
+                                if (containerRef.current == null) {
+                                    return translate 
+                                }
+                                const newTranslate = translate + TRANSLATE_AMOUT
+                                const edge = containerRef.current.scrollWidth
+                                const width = containerRef.current.clientWidth
+                                if (newTranslate + width >= edge) {
+                                    return edge - width
+                                }
+                                return newTranslate 
+                            })
+                        }}>
+                    <ChevronRight />
+                </Button>
+            </div>
+        )}
     </div>
   )
 }
