0057e2adb9fd7cc5e787694df95788fee66cf7af
Dušan Jarkovský <jarkovsky.dev@gmail.com>
2024-01-15 23:40:24 +0100

    1:11:27 Sidebar next

--- src/utils/formatDuration.ts
+++ src/utils/formatDuration.ts
@@ -1,16 +1,17 @@
-import { Divide } from 'lucide-react'
-import React from 'react'
+const LEADING_ZERO_FORMATTER = new Intl.NumberFormat(undefined, {
+  minimumIntegerDigits: 2,
+})
 
+export function formatDuration(duration: number) {
+  const hours = Math.floor(duration / 60 / 60)
+  const minutes = Math.floor((duration - hours * 60 * 60) / 60)
+  const seconds = duration % 60
 
-const LEADING_ZERO_FORMATTER = new Intl.NumberFormat(undefined, {minimumIntegerDigits: 2})
+  if (hours > 0) {
+    return `${hours}:${LEADING_ZERO_FORMATTER.format(
+      minutes
+    )}:${LEADING_ZERO_FORMATTER.format(seconds)}`
+  }
 
-
-const formatDuration = (duration: number) => {
-    const hour = Math.floor(duration / 60 / 60)
-    const minutes 
-  return (
-    null
-  )
+  return `${minutes}:${LEADING_ZERO_FORMATTER.format(seconds)}`
 }
-
-export default formatDuration
