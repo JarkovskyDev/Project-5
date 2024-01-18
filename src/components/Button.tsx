7dd7d29613b0cbd9a6baad7eb6503226eebd8688
Dušan Jarkovský <jarkovsky.dev@gmail.com>
2024-01-18 23:47:56 +0100

    end

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,40 +1,44 @@
-import { VariantProps, cva } from 'class-variance-authority'
+import { VariantProps, cva } from "class-variance-authority"
 import { ComponentProps } from "react"
-import { twMerge } from 'tailwind-merge'
+import { twMerge } from "tailwind-merge"
 
-const buttonStyles = cva(["transition-colors"], {
-    variants: {
-        variant: {
-            default: ["bg-secondary", "hover:bg-secondary-hover"],
-            ghost: ["hover:bg-gray-100"],
-            dark: ["bg-secondary-dark", "hover:bg-secondary-dark-hover", "text:secondary"]
-        },
-        defaultVariants: {
-            variants: "default",
-            size: "default"
-        },
-        size: {
-            default: ["rounded", "p-2"],
-            icon: [
-                "rounded-full", 
-                "w-10", 
-                "h-10", 
-                "flex", 
-                "items-center", 
-                "justify-center", 
-                "p-2.5"
-            ],
-        },
+export const buttonStyles = cva(["transition-colors"], {
+  variants: {
+    variant: {
+      default: ["bg-secondary", "hover:bg-secondary-hover"],
+      ghost: ["hover:bg-gray-100"],
+      dark: [
+        "bg-secondary-dark",
+        "hover:bg-secondary-dark-hover",
+        "text-secondary",
+      ],
     },
+    size: {
+      default: [" rounded", "p-2"],
+      icon: [
+        "rounded-full",
+        "w-10",
+        "h-10",
+        "flex",
+        "items-center",
+        "justify-center",
+        "p-2.5",
+      ],
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
 })
 
 type ButtonProps = VariantProps<typeof buttonStyles> & ComponentProps<"button">
 
-const Button = ({variant, size, className, ...props }: ButtonProps) => {
+export function Button({ variant, size, className, ...props }: ButtonProps) {
   return (
-    <button {...props} 
-    className={twMerge(buttonStyles({ variant, size}), className)}/>
+    <button
+      {...props}
+      className={twMerge(buttonStyles({ variant, size }), className)}
+    />
   )
 }
-
-export default Button
